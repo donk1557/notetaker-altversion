41d7c617cd3962f04ddae12e2cec24fb38061e78
Donovan K <7donovan7@gmail.com>
2021-11-04 02:33:35 -0400

    route api

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
-// const router = require('express').Router();
+const router = require('express').Router();
 
 const path = require('path');
 const port = process.env.PORT || 4000;
 const app = express();
-const api = require('./public/js/index.js');
+// const api = require('./public/js/index.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
+
+
+
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+  
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-
-
 app.listen(port, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${port}`);
   });
+
+  
+  module.exports = router;
